cccd6590160975fcf2f0f49d5c6e5476863c83d4
Caio Corsini <caiofilho14@gmail.com>
2024-08-25 19:04:50 -0300

    postagem tarefa lab 1

--- lab01b/ex4.c
+++
@@ -1,4 +0,0 @@
-/*
-4. Crie um progama capaz de ler dois nomes de pessoas e imprimi-los em ordem alfabética. Faça isto com string de C e de C++
-*/
-
--- lab01b/ex5.c
+++
@@ -1,5 +0,0 @@
-/*
-5. Crie um programa  capaz de multiplicar uma linha de uma matriz de inteiros por um dado número.
-Faça o mesmo para uma coluna. A matriz deve ser lida de teclado.
-*/
-
