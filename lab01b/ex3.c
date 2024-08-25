920f5d3a4506b25b753c6dd872383566d8580795
Alan Gleizer <alan@mgleizer.com>
2024-08-25 13:28:19 -0300

    ex 3 finalizado

--- lab01b/ex3.c
+++ lab01b/ex3.c
@@ -8,3 +8,45 @@ Este número deve ser sempre ímpar.
         5   
 */
 
+#include <stdio.h>
+#include <math.h>
+
+void imprimirLinha(int vetor[], int indiceInicial, int indiceFinal) {
+  // imprimir espaços
+  if (indiceInicial != 0) {
+    for (int i = (indiceInicial) * 2; i > 0; i--) {
+      printf(" ");
+    }
+  }
+
+  // imprimir valores
+  for (int i = indiceInicial; i <= indiceFinal; i++) {
+    printf("%d ", vetor[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+
+  // determinacao numero maximo
+  int numMax;
+  do {
+  printf("Informe o numero maximo (ímpar!): ");
+  scanf("%d", &numMax);
+  if (numMax < 1 || numMax % 2 == 0) printf("Numero invalido!\n");
+  } while (numMax < 1 || numMax % 2 == 0);
+
+
+  // criacao vetor
+  int vetor[numMax];
+  for (int i = 0; i < numMax; i++) {
+    vetor[i] = i+1;
+  }
+
+  // impressao formatada
+  int numeroLinhas = (int)(ceil(numMax / 2.0));
+
+  for (int i = 0; i < numeroLinhas; i++) {
+    imprimirLinha(vetor, i, numMax - i - 1);
+    }
+  }
