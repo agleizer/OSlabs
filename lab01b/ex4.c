3718b4c0ece8b769b3ed31c7001098dbde2cb674
Alan Gleizer <alan@mgleizer.com>
2024-08-25 12:27:22 -0300

    inclusao enunciados lab01b

---
+++ lab01b/ex4.c
@@ -0,0 +1,4 @@
+/*
+4. Crie um progama capaz de ler dois nomes de pessoas e imprimi-los em ordem alfabética. Faça isto com string de C e de C++
+*/
+
---
+++ lab01b/ex5.c
@@ -0,0 +1,5 @@
+/*
+5. Crie um programa  capaz de multiplicar uma linha de uma matriz de inteiros por um dado número.
+Faça o mesmo para uma coluna. A matriz deve ser lida de teclado.
+*/
+
